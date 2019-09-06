4160389cdf16ef03a316c3cfbe33599b163e24af
Gabriel Miranda <gabrielrumiranda@gmail.com>
2019-09-06 15:58:28 -0300

    Refactor validade push

--- app/line.rb
+++ app/line.rb
@@ -16,14 +16,32 @@ class Line
     @tokens.push(value)
   end
 
-  def validate_push(_value)
-    last_numbers = @tokens.last(3)
-    if last_numbers.size < 3
-      true
-    elsif last_numbers.size >= 3
-      last_numbers.uniq.size > 1
+  def validate_push(value)
+    if ROMAN_VALUES.index(value)
+      validade_roman_precedence(value)
     else
-      false
+      true
+    end
+  end
+
+  def validade_roman_precedence(value)
+    index_value = ROMAN_VALUES.index(value)
+    index_last_token = ROMAN_VALUES.index(@tokens.last)
+    if index_last_token
+      if index_last_token > index_value
+        true
+      elsif index_last_token < index_value
+        index_last_token + 1 == index_value
+      else
+        last_numbers = @tokens.last(3)
+        if last_numbers.size < 3
+          true
+        else
+          last_numbers.uniq.size > 1
+        end
+      end
+    else
+      true
     end
   end
 
