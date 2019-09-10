3b4a85231f9bdf17995e2e62a6e11e79ae5d55ba
Gabriel Miranda <gabrielrumiranda@gmail.com>
2019-09-10 11:26:01 -0300

    Create dictionary unit tests

---
+++ spec/app/dictionary_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../spec_helper.rb'
+RSpec.describe 'Dictionary' do
+  describe '.add_word' do
+    context 'Words is empty and add some word' do
+      subject { Dictionary.new }
+
+      it 'Returns an hash with one element' do
+        subject.add_word('Plact', 52)
+        expect(subject.words).not_to be_empty
+        expect(subject.words).to include('Plact' => 52)
+        expect(subject).to have_exactly(1).words
+      end
+    end
+
+    context 'Words is not empty and add some word' do
+      subject { Dictionary.new }
+
+      before(:example) do
+        subject.add_word('Plact', 52)
+      end
+
+      it 'Returns an hash with one element' do
+        subject.add_word('Zum', 30)
+        expect(subject.words).not_to be_empty
+        expect(subject.words).to include('Plact' => 52)
+        expect(subject.words).to include('Zum' => 30)
+        expect(subject).to have_exactly(2).words
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
+require 'rspec/collection_matchers'
+
 require_relative '../app/line.rb'
 require_relative '../app/token.rb'
-require 'rspec/collection_matchers'
 require_relative '../app/calculator.rb'
+require_relative '../app/dictionary.rb'
