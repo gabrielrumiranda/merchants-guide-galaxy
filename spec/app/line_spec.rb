fcf2b0874d1a150591338dad846081654fa22128
Gabriel Miranda <gabrielrumiranda@gmail.com>
2019-09-16 15:58:23 -0300

    Refactor line_spec

--- spec/app/line_spec.rb
+++ spec/app/line_spec.rb
@@ -7,44 +7,45 @@ RSpec.describe 'Line' do
   let(:roman_v) { Token.new('V', 5) }
   let(:roman_x) { Token.new('X', 10) }
   let(:galaxy_number) { Token.new('glub', 2) }
+  let(:line) { Line.new }
 
   describe '.push' do
-    context 'when token is empty' do
-      subject(:line) { Line.new }
+    subject(:push) { line.push(token) }
 
-      before do
-        line.push(roman_x)
-      end
+    context 'when token is empty' do
+      let(:token) { roman_x }
 
       it 'Line.tokens is equal a array with the pushed tokens' do
-        expect(subject.tokens).to eq([roman_x])
+        expect(push).to eq([roman_x])
       end
     end
 
     context 'Tokens is not empty and push some token' do
-      subject(:line) { Line.new }
+      let(:token) { roman_x }
 
       before do
         line.push(roman_i)
-        line.push(roman_x)
+        push
       end
 
-      it 'Line.tokens is equal a array with the two pushed tokens' do
-        expect(line.tokens).to eq([roman_i, roman_x])
+      it 'Returns a array with the two pushed tokens' do
+        expect(push).to eq([roman_i, roman_x])
       end
     end
   end
 
   describe '.add_buffer' do
-    context 'Tokens is empty and push some token to buffer' do
-      subject(:line) { Line.new }
+    subject(:add_buffer) { line.add_buffer(token) }
 
-      before do
-        line.add_buffer(roman_x)
-      end
+    context 'Tokens is empty and push some token to buffer' do
+      let(:token) { roman_x }
 
       it 'Line.tokens is equal the added token' do
-        expect(line.tokens).to eql([roman_x])
+        expect(add_buffer).to eql([roman_x])
+      end
+
+      before do
+        add_buffer
       end
 
       it 'Line.accumulate is equal 10' do
@@ -53,84 +54,85 @@ RSpec.describe 'Line' do
     end
 
     context 'Have a Roman number in buffer and push a bigger roman number' do
-      subject(:line) { Line.new }
+      let(:token) { roman_v }
 
       before do
         line.add_buffer(roman_i)
-        line.add_buffer(roman_v)
+        add_buffer
       end
 
       it 'Line.tokens is equal the added tokens' do
-        expect(subject.tokens).to eq([roman_i, roman_v])
+        expect(add_buffer).to eq([roman_i, roman_v])
       end
 
       it 'Line.accumulate is equal 4' do
-        expect(subject.accumulate).to eq(4)
+        expect(line.accumulate).to eq(4)
       end
     end
 
     context 'Have a Roman number in buffer and push a smaller roman number' do
-      subject(:line) { Line.new }
+      let(:token) { roman_v }
 
       before do
         line.add_buffer(roman_x)
-        line.add_buffer(roman_v)
+        add_buffer
       end
 
       it 'Line.tokens is equal the added tokens' do
-        expect(subject.tokens).to eq([roman_x, roman_v])
+        expect(add_buffer).to eq([roman_x, roman_v])
       end
 
       it 'Line.accumulate is equal 15' do
-        expect(subject.accumulate).to eq(15)
+        expect(line.accumulate).to eq(15)
       end
     end
 
     context 'Have a Roman number in buffer and push a galaxy number' do
-      subject(:line) { Line.new }
+      let(:token) { galaxy_number }
 
       before do
         line.add_buffer(roman_x)
-        line.add_buffer(galaxy_number)
+        add_buffer
       end
 
       it 'Line.tokens is equal the added tokens' do
-        expect(subject.tokens).to eq([roman_x, galaxy_number])
+        expect(add_buffer).to eq([roman_x, galaxy_number])
       end
 
       it 'Line.accumulate is equal 20' do
-        expect(subject.accumulate).to eq(20)
+        expect(line.accumulate).to eq(20)
       end
     end
 
     context 'Have three equal Roman number in buffer and push a the same number' do
-      subject(:line) { Line.new }
+      let(:token) { roman_x }
 
       before do
         line.add_buffer(roman_x)
         line.add_buffer(roman_x)
         line.add_buffer(roman_x)
+        add_buffer
       end
 
-      it 'Line.tokens is equal the added tokens' do
-        expect(line.tokens).to eq([roman_x, roman_x, roman_x])
+      it 'Returns nil' do
+        expect(add_buffer).to eq(nil)
       end
 
       it 'Line.accumulate is equal 30' do
-        expect(subject.accumulate).to eq(30)
+        expect(line.accumulate).to eq(30)
       end
     end
 
     context 'Have a Roman number in buffer and push an invalid roman number' do
-      subject(:line) { Line.new }
+      let(:token) { roman_x }
 
       before do
         line.add_buffer(roman_i)
-        line.add_buffer(roman_x)
+        add_buffer
       end
 
-      it 'Line.tokens is equal the first token pushed' do
-        expect(line.tokens).to eq([roman_i])
+      it 'Returns nil' do
+        expect(add_buffer).to eq(nil)
       end
 
       it 'Line.accumulate is equal 1' do
